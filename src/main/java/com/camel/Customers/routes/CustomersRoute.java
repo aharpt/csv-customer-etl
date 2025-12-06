d3b559228e01fdd435d52b9aeb2cc35e1bfa259c
Aaron Harpt <aaron.harpt@gmail.com>
2025-12-06 15:21:39 -0600

    Remove unused imports

--- src/main/java/com/camel/Customers/routes/CustomersRoute.java
+++ src/main/java/com/camel/Customers/routes/CustomersRoute.java
@@ -1,12 +1,9 @@
 package com.camel.Customers.routes;
 
-import com.camel.Customers.models.Customer;
 import com.camel.Customers.utility.CustomerMapper;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.springframework.stereotype.Component;
-import tools.jackson.dataformat.csv.CsvMapper;
-import tools.jackson.dataformat.csv.CsvSchema;
 
 @Component
 public class CustomersRoute extends RouteBuilder {
